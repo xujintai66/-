43695cc8d52b42a6ddb56060d040c42fcc24691d
xujintai <1045752930@qq.com>
2024-12-16 23:40:06 +0800

    将接口地址改为公网ip

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://43.139.233.117:8080',
   timeout: 5000,
   headers: {
     platform: 'H5',
